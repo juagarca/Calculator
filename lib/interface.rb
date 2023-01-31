41eaad7d2655f485ed47f07ed391f57acef18743
Juan Garcia <juagarca@gmail.com>
2023-01-31 19:32:18 +0000

    Add first step

---
+++ lib/interface.rb
@@ -0,0 +1,26 @@
+# 1. Say hello to the user
+
+# 2. Ask user for the first number
+
+# 3. Get user answer
+
+# 4. Ask user for second number
+
+# 5. Get user answer
+
+# 6. Ask user for mathematical operation
+
+# 7. Get user answer
+
+# 8. Perfom the operation
+
+# 9. Show the result to the user
+
+# OUTPUT
+# Enter a first number:
+# > 6
+# > Enter a second one:
+# > 8
+# > Choose operation [+][-][*][/]
+# > *
+# > Result: 48
