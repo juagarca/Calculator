efca57f675d74d165cfe88c59eec2fff52ccbf0e
Juan Garcia <juagarca@gmail.com>
2023-01-31 20:10:54 +0000

    Second push

--- lib/interface.rb
+++ lib/interface.rb
@@ -1,26 +1,34 @@
 # 1. Say hello to the user
+puts 'Hello'
 
 # 2. Ask user for the first number
+puts 'Enter a first number:'
 
 # 3. Get user answer
+first_number = gets.chomp.to_i
 
 # 4. Ask user for second number
+puts 'Enter a second one:'
 
 # 5. Get user answer
+second_number = gets.chomp.to_i
 
 # 6. Ask user for mathematical operation
+puts 'Choose operation [+][-][*][/]'
 
 # 7. Get user answer
+operator = gets.chomp
+puts '---------'
 
 # 8. Perfom the operation
+result = 0
+
+case operator
+when '+' then result = first_number + second_number
+when '-' then result = first_number - second_number
+when '*' then result = first_number * second_number
+when '/' then result = first_number / second_number.to_f
+end
 
 # 9. Show the result to the user
-
-# OUTPUT
-# Enter a first number:
-# > 6
-# > Enter a second one:
-# > 8
-# > Choose operation [+][-][*][/]
-# > *
-# > Result: 48
+puts "Result: #{result}"
